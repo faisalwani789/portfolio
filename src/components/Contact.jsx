e1a4b1dc6885615148664093ba129246fd37b504
Faisal yousuf wani <faisalyousuf1122@gmail.com>
2025-11-16 22:27:28 +0530

    added react-hook-form for contact page validation

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,22 +4,28 @@ import TitleColor from './TitleColor'
 import ContactCard from './ContactCard'
 import { buttonSocialData, contactCardData, FormInfo,FormInfo2 } from '../Utils/Constants'
 import SocialButton from './SocialButton'
-import LinearBgButton from "./LinearBgButton"
 import InputBox from './InputBox'
 import IconButton from './IconButton'
 import { FaTelegram } from 'react-icons/fa'
 import Section from './Section'
-import { useRef } from 'react'
 import Popup from './Popup'
+import {useForm} from 'react-hook-form'
 const Contact = () => {
+  const {register,handleSubmit,formState:{errors}}=useForm()
   const [messageSent, setMessageSent] = useState(false)
-  const [message, setMessage] = useState("")
-  const[error,setError]=useState(false)
+  const [successMsg, setSuccessMsg] = useState("")
+  
+  const[submitting,setSubmitting]=useState(false)
   // const formRef=useRef(null)
-  const nameRef = useRef(null)
-  const messageRef = useRef(null)
-  const subjectRef = useRef(null)
-  const emailRef = useRef(null)
+  // const nameRef = useRef(null)
+  // const messageRef = useRef(null)
+  // const subjectRef = useRef(null)
+  // const emailRef = useRef(null)
+
+  // const email=watch('email')
+  // const name=watch('name')
+  // const subject=watch('subject')
+  // const message=watch('message')
 
   // const submit=(e)=>{
   //   e.preventDefault()
@@ -27,35 +33,37 @@ const Contact = () => {
   //   const values=FormInfo2.map((ele)=>data.get(ele.name)?? "")
   //   alert(JSON.stringify(values))
   // }
-  const handleSubmitMessage = async (e) => {
-    e.preventDefault()
+  const handleSubmitMessage = async (data) => {
+    // e.preventDefault()
+    console.log('form data'+data)
+    setSubmitting(true)
+    
     try {
       const res = await fetch('https://netmaxgptbkd-1.onrender.com/api/message', {
       method: 'Post',
       headers: {
         'Content-Type': 'application/json'
       },
-      body: JSON.stringify({
-        Name: nameRef.current.value,
-        Email: emailRef.current.value,
-        Subject: subjectRef.current.value,
-        Message: messageRef.current.value,
-      })
+      body: JSON.stringify(data)
     })
     const json = await res.json()
     console.log(json)
     if (json.success) {
       setMessageSent(true)
-      setMessage(json.message)
+      setSuccessMsg(json.message)
+     
     }
-      nameRef.current.value = ""
-      emailRef.current.value = ""
-      subjectRef.current.value = ""
-      messageRef.current.value = ""
+      // nameRef.current.value = ""
+      // emailRef.current.value = ""
+      // subjectRef.current.value = ""
+      // messageRef.current.value = ""
       console.log(json)
 
     } catch (error) {
-      setError(error.message)
+      console.log(error)
+    }
+    finally{
+       setSubmitting(false)
     }
     
    
@@ -82,25 +90,29 @@ const Contact = () => {
           </div>
           <div>
             <h2 className='font-bold text-2xl mb-6'>Send Me a Message</h2>
-            <form className='space-y-6 ' onSubmit={handleSubmitMessage} >
+            <form className='space-y-6 ' onSubmit={handleSubmit(handleSubmitMessage)} >
               <div className='grid grid-cols-1 md:grid-cols-2 gap-6'>
                 {/* {FormInfo2.map(input=><InputBox key={input.id} name={input.name} text={input.text}/>)} */}
-                <InputBox ref={nameRef} name={'Name'} text={'Your Name'} />
-                <InputBox ref={emailRef} name={'Email'} text={'Your Email'} />
+                <InputBox {...register('name',{required:"Name is required"})}  error = {errors.name?.message} name={'Name'} label={'Your Name'} type={'text'}  />
+               
+                <InputBox {...register('email',{required:'email is required',pattern:{value:/^[^\s@]+@[^\s@]+\.[^\s@]+$/,message:'invalid email'}})}  error = {errors.email?.message}  name={'Email'} label={'Your Email'} type={"email"} />
+                
               </div>
 
               <div>
-                <InputBox ref={subjectRef} name={'Subject'} text={'Subject'} />
+                <InputBox {...register('subject',{required:"subject is required"})}  error = {errors.subject?.message} name={'Subject'} label={'Subject'} type={'text'} />
                 <div>
                   <label htmlFor='Message' className='mb-2 block'>Your Message</label>
-                  <textarea ref={messageRef} style={{ height: '123px' }} className='contact-input w-full rounded-lg focus:outline-none ' rows={'5'} id='Message' name='Message' required></textarea>
+                  <textarea {...register("msg",{required:"msg is required"})} style={{ height: '123px' }} className='contact-input w-full rounded-lg focus:outline-none ' rows={'5'} id='Message' name='Message' ></textarea>
+                  {errors.msg &&<p>{errors.msg?.message}</p>}
                 </div>
               </div>
-              <IconButton variant={'primary'} className='flex gap-2 items-center justify-center w-full' icon={FaTelegram}>Send Message</IconButton>
+              <IconButton variant={'primary'} type={'submit'} className='flex gap-2 items-center justify-center w-full' icon={FaTelegram} >Send Message</IconButton>
             </form>
           </div>
         </div>
-        {messageSent && <Popup messageSent={messageSent} setMessageSent={setMessageSent} setMessage={setMessage} message={message} error={error} ></Popup>}
+    
+        {messageSent && <Popup messageSent={messageSent} setMessageSent={setMessageSent} setMessage={setSuccessMsg} successMsg={successMsg}  submitting={submitting} ></Popup>}
       </Section>
     )
   }
--- src/components/InputBox.jsx
+++ src/components/InputBox.jsx
@@ -1,11 +1,12 @@
 import React, { forwardRef} from 'react'
 
-const InputBox = forwardRef(({name,text},inputRef) => {
+const InputBox = forwardRef(({name,label,error,...rest},inputRef) => {
  
   return (
     <div>
-         <label htmlFor={name} className='block mb-2 font-medium'>{text}</label>
-        <input ref={inputRef} className=' contact-input rounded-lg w-full focus:outline-none' required id={name} name={name} type="text"/>
+         <label htmlFor={name} className='block mb-2 font-medium'>{label}</label>
+        <input ref={inputRef} {...rest} className=' contact-input rounded-lg w-full focus:outline-none' />
+        {error && <p>{error}</p>}
     </div>
   )
 })
